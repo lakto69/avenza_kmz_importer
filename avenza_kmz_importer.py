38e3206c3edab69270ffcddf5d8f880c1a01b9a7
Andrés de M. Leite <leite.m.andres@gmail.com>
2025-10-15 09:38:46 -0300

    Qt6 compat. I

--- avenza_kmz_importer.py
+++ avenza_kmz_importer.py
@@ -25,7 +25,7 @@
  ***************************************************************************/
 """
 # Bibliotecas:
-from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt, QSettings
+from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt
 from qgis.PyQt.QtGui import QIcon, QColor
 from qgis.PyQt.QtWidgets import QToolButton, QAction, QFileDialog # CAIXA DE DIÁLOGO
 from qgis.core import QgsProject, QgsVectorLayer, QgsSymbol, QgsSvgMarkerSymbolLayer, QgsCategorizedSymbolRenderer, QgsRendererCategory, QgsLineSymbol, QgsFillSymbol, QgsPalLayerSettings, QgsVectorLayerSimpleLabeling
@@ -334,7 +334,7 @@ class AvenzaKMZImporter:
         # show the dialog
         self.dlg.show()
         # Run the dialog event loop
-        result = self.dlg.exec_()
+        result = self.dlg.exec()
         # See if OK was pressed
         if result:
             # Do something useful here - delete the line containing pass and
