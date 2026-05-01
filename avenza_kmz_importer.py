d49468d958181bd84d1b827982c2b4e67314ec68
Andrés de M. Leite <leite.m.andres@gmail.com>
2026-05-01 17:01:07 -0300

    add 'images' field

--- avenza_kmz_importer.py
+++ avenza_kmz_importer.py
@@ -112,7 +112,7 @@ class AvenzaKMZImporter:
     def setInicial(self):
         self.simbologia = None
         self.tree = None
-        self.point_cols = ['Name', 'geometry', 'Time', 'Style', 'Notes', 'Icon_URL', 'Icon_local']
+        self.point_cols = ['Name', 'geometry', 'Time', 'Style', 'Notes', 'Icon_URL', 'Icon_local', 'images']
         self.esquemas = {}
 
 
@@ -375,13 +375,13 @@ class AvenzaKMZImporter:
         # converte gpd em geojson
         json_camada = gdf_camada.to_json()
 
-        layer_add = QgsVectorLayer(json_camada, nome_camada, 'ogr') # ? repetida
+        layer_add = QgsVectorLayer(json_camada, nome_camada, 'ogr')
 
-        # Defina a coluna a ser categorizada # ? repetida
-        column_name = 'Style' # ? repetida
+        # Defina a coluna a ser categorizada
+        column_name = 'Style'
 
-        # Crie a classe de simbologia categorizada # ? repetida
-        renderer = QgsCategorizedSymbolRenderer(column_name, []) # ? repetida
+        # Crie a classe de simbologia categorizada
+        renderer = QgsCategorizedSymbolRenderer(column_name, [])
         
         # Processando a simbologia da camada
         if tipo_camada=='Point':
@@ -436,20 +436,20 @@ class AvenzaKMZImporter:
                 category = QgsRendererCategory(estilo, fill_symbol, estilo)
                 renderer.addCategory(category)
 
-        # Atribua a simbologia à camada # ? repetida
-        layer_add.setRenderer(renderer) # ? repetida
+        # Atribua a simbologia à camada
+        layer_add.setRenderer(renderer)
 
         # Mostra a contagem de elementos
         layer_add.setCustomProperty("showFeatureCount", True) 
 
-        # Atualize a exibição da camada # ? repetida
-        layer_add.triggerRepaint() # ? repetida
+        # Atualize a exibição da camada
+        layer_add.triggerRepaint()
 
-        # Adicionar as camadas ao projeto do QGIS # ? repetida
-        QgsProject.instance().addMapLayer(layer_add, False) # ? repetida
+        # Adicionar as camadas ao projeto do QGIS
+        QgsProject.instance().addMapLayer(layer_add, False)
 
-        # Adicionando as camadas em um grupo: # ? repetida
-        grupo.addLayer(layer_add) # ? repetida
+        # Adicionando as camadas em um grupo:
+        grupo.addLayer(layer_add)
 
         # Rotular feições
         if self.dlg.checkBoxRotularNome.isChecked():
@@ -568,18 +568,18 @@ class AvenzaKMZImporter:
                         self.add_log(self.tr(u'Error processing geometry.'), f'{self.tr(u"Current layer")}: {camada_atual}.<br>{self.tr(u"Columns")}: {self.point_cols}<br>{self.tr(u"Points")}: {str(points)} <br>{self.tr(u"Error")}: <font color="#e92121">{str(e)}</font>')
                 if lines!=[]:
                     try:
-                        df_lines = pd.DataFrame(lines, columns=['Name', 'geometry', 'Time', 'Style', 'Notes', 'Line Color', 'Line Width', 'Line Opacity'])    
+                        df_lines = pd.DataFrame(lines, columns=['Name', 'geometry', 'Time', 'Style', 'Notes', 'Line Color', 'Line Width', 'Line Opacity', 'images'])    
                         self.add_df_to_qgis(df_lines, 'Lines', self.simbologia, 'LineString', camada_atual)
                     except Exception as e:
                         self.cursor_arrow()
-                        self.add_log(self.tr(u'Error processing geometry.'), f'{self.tr(u"Current layer")}: {camada_atual}.<br>{self.tr(u"Columns")}: {["Name", "geometry", "Time", "Style", "Notes", "Line Color", "Line Width", "Line Opacity"]}<br>{self.tr(u"Lines")}: {str(lines)} <br>{self.tr(u"Error")}: <font color="#e92121">{str(e)}</font>')
+                        self.add_log(self.tr(u'Error processing geometry.'), f'{self.tr(u"Current layer")}: {camada_atual}.<br>{self.tr(u"Columns")}: {["Name", "geometry", "Time", "Style", "Notes", "Line Color", "Line Width", "Line Opacity", 'images']}<br>{self.tr(u"Lines")}: {str(lines)} <br>{self.tr(u"Error")}: <font color="#e92121">{str(e)}</font>')
                 if polygons!=[]:
                     try:
-                        df_polygons = pd.DataFrame(polygons, columns=['Name', 'geometry', 'Time', 'Style', 'Notes', 'Line Color', 'Line Width', 'Line Opacity', 'Polygon Color', 'Polygon Opacity'])
+                        df_polygons = pd.DataFrame(polygons, columns=['Name', 'geometry', 'Time', 'Style', 'Notes', 'Line Color', 'Line Width', 'Line Opacity', 'Polygon Color', 'Polygon Opacity', 'images'])
                         self.add_df_to_qgis(df_polygons, 'Polygons', self.simbologia, 'Polygon', camada_atual)
                     except Exception as e:
                         self.cursor_arrow()
-                        self.add_log(self.tr(u'Error processing geometry.'), f'{self.tr(u"Current layer")}: {camada_atual}.<br>{self.tr(u"Columns")}: {["Name", "geometry", "Time", "Style", "Notes", "Line Color", "Line Width", "Line Opacity", "Polygon Color", "Polygon Opacity"]}<br>{self.tr(u"Polygons")}: {str(polygons)} <br>{self.tr(u"Error")}: <font color="#e92121">{str(e)}</font>')
+                        self.add_log(self.tr(u'Error processing geometry.'), f'{self.tr(u"Current layer")}: {camada_atual}.<br>{self.tr(u"Columns")}: {["Name", "geometry", "Time", "Style", "Notes", "Line Color", "Line Width", "Line Opacity", "Polygon Color", "Polygon Opacity", 'images']}<br>{self.tr(u"Polygons")}: {str(polygons)} <br>{self.tr(u"Error")}: <font color="#e92121">{str(e)}</font>')
 
             # Expande ou não o conteúdo do grupo
             self.setExpanded(self.dlg.checkBoxExpandirFeicoes.isChecked())
@@ -589,6 +589,7 @@ class AvenzaKMZImporter:
 
         if len(tree.xpath('//kml:Folder', namespaces={'kml': self.t[1:-1]}))==0:
             self.add_log(self.tr(u'Error'), self.tr(u'No layer found to process.'))
+    
     def process_placemarks(self, camada, tree):
         # Listas para armazenar feições por tipo
         points = []
@@ -611,6 +612,12 @@ class AvenzaKMZImporter:
             notes = {}
             for nota in placemark.findall(f'./*//{self.t}SimpleData'):
                 notes[nota.attrib.values()[0]] = nota.text
+            # images
+            if notes.get('Photo Name') is not None:
+                images = notes.get('Photo Name')
+            else:                
+                images = None
+
             # Name
             feature_name = placemark.find(f'{self.t}name')
             if feature_name != None:
@@ -651,20 +658,20 @@ class AvenzaKMZImporter:
             if feature_type == 'Point':
                 geometry = Point(coordinates[0][0], coordinates[0][1], coordinates[0][2])
                 if self.esquemas.get('track_schema') is not None:
-                    points.append(tuple([feature_name, geometry, time, urlstyle, notes, icon_url, icon_local] + [None, None] + [None for x in list(self.esquemas['track_schema'].keys())]))
+                    points.append(tuple([feature_name, geometry, time, urlstyle, notes, icon_url, icon_local, images] + [None, None] + [None for x in list(self.esquemas['track_schema'].keys())]))
                 else:
-                    points.append(tuple([feature_name, geometry, time, urlstyle, notes, icon_url, icon_local]))
+                    points.append(tuple([feature_name, geometry, time, urlstyle, notes, icon_url, icon_local, images]))
 
             elif feature_type == 'Polygon':
                 try:
                     geometry = Polygon(coordinates)
-                    polygons.append((feature_name, geometry, time, urlstyle, notes, cor_linha, espessura_linha, opacidade_linha, cor_fundo, opacidade_fundo))
+                    polygons.append((feature_name, geometry, time, urlstyle, notes, cor_linha, espessura_linha, opacidade_linha, cor_fundo, opacidade_fundo, images))
                 except Exception as e:
                     self.add_log(self.tr(u'Error processing geometry.'), f'{self.tr(u"Feature")}: {feature_name}.<br>{self.tr(u"Coordinates")}: {str(coordinates)} <br>{self.tr(u"Error")}: <font color="#e92121">{str(e)}</font>')
             elif feature_type == 'LineString':
                 try:
                     geometry = LineString(coordinates)
-                    lines.append((feature_name, geometry, time, urlstyle, notes, cor_linha, espessura_linha, opacidade_linha))   
+                    lines.append((feature_name, geometry, time, urlstyle, notes, cor_linha, espessura_linha, opacidade_linha, images))   
                 except Exception as e:
                     self.add_log(self.tr(u'Error processing geometry.'), f'{self.tr(u"Feature")}: {feature_name}.<br>{self.tr(u"Coordinates")}: {str(coordinates)} <br>{self.tr(u"Error")}: <font color="#e92121">{str(e)}</font>')
             elif feature_type == 'Track':
@@ -672,11 +679,11 @@ class AvenzaKMZImporter:
                 point_geometry, line_geometry, array_data = self.extract_track_data(placemark)
                 # Cria a linha
                 if line_geometry is not None:
-                    lines.append((feature_name, line_geometry[0], time, urlstyle, notes, cor_linha, espessura_linha, opacidade_linha))   
+                    lines.append((feature_name, line_geometry[0], time, urlstyle, notes, cor_linha, espessura_linha, opacidade_linha, images))   
                 # cria os pontos
                 # Junta os dados Placemark + geometria de pontos + array_data:
                 for i, geometry_pt in enumerate(point_geometry):
-                    reg = tuple([feature_name, geometry_pt, time, urlstyle, notes, icon_url, icon_local] + list(array_data.loc[i]))
+                    reg = tuple([feature_name, geometry_pt, time, urlstyle, notes, icon_url, icon_local, images] + list(array_data.loc[i]))
                     points.append(reg)
             else:
                 pass
